e81539baf7e4f3a1b68dba746a7de9a8b59bc15e
JoaoPotencia <joaosantosrai@gmail.com>
2023-10-27 14:23:24 -0300

    Update 27/10

---
+++ js/testeAnimacao.js
@@ -0,0 +1,35 @@
+function mudarAnimacao(event) {
+
+    let listaAni = document.querySelectorAll("#ani")
+
+    if (event.target.id == "previous") {
+        listaAni.forEach(elemento => {
+            elemento.className = "animated slideInRight"
+        })
+    } else if (event.target.id == "next") {
+        listaAni.forEach(elemento => {
+            elemento.className = "animated slideInLeft"
+        })
+    }
+
+
+}
+
+
+
+function mudarAnimacao(event) {
+
+    let listaAni = document.querySelectorAll("#aniBtn")
+
+    if (event.target.id == "previous") {
+        listaAni.forEach(elemento => {
+            elemento.className = "animated slideInRight"
+        })
+    } else if (event.target.id == "next") {
+        listaAni.forEach(elemento => {
+            elemento.className = "animated slideInLeft"
+        })
+    }
+
+
+}
